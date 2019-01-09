8e8d76ac9882a797cde782511b27d612367acde9
Benjamin Chen <benjamin9527@icloud.com>
2019-01-08 16:13:39 -0800

    atart adding recycle view

--- app/src/main/java/com/example/t00062765/implocalsqlite/MainActivity.java
+++ app/src/main/java/com/example/t00062765/implocalsqlite/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.t00062765.implocalsqlite;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    //add recycle view
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     DBAdapter myDb;
 
@@ -14,6 +20,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //recycle view
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
 
         openDB();
     }
---
+++ app/src/main/java/com/example/t00062765/implocalsqlite/MyAdapter.java
@@ -0,0 +1,13 @@
+package com.example.t00062765.implocalsqlite;
+
+import android.support.v7.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+    public MyAdapter(){
+
+    }
+
+    public MyAdapter.MyViewHolder onCreateViewHlder(){
+
+    }
+}
